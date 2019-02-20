8f072a8652538ec92c62f7298e240c7291961d60
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-20 10:25:27 +0800

    Saving data 'add a Gig'

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
@@ -23,6 +25,28 @@ namespace GigHub.Controllers
             };
 
             return View(ViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
