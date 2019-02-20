10f62b0f9b28998e1fc9c1387bca22b2a8c8d367
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-20 09:49:08 +0800

    Add Genre DownList and Button with bootstrap style

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var ViewModel = new GigFormViewModel
+            {
+                Genres= _context.Genres.ToList()
+            };
+
+            return View(ViewModel);
         }
     }
 }
