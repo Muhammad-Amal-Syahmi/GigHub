4c59ee0fbad8dd26573cf8f5d56cd6e49d9ec017
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-02-26 14:12:29 +0800

    Improve the looks and feels of homepage

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace GigHub.Controllers
         {
             var upcomings = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcomings);
         }
